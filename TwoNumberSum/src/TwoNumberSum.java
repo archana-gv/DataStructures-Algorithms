5dcd4571ec5e26b8bac4e3f3bb176a2abee7c203
Archana Garudanagiri Vidyashankar <gvarchana95@gmail.com>
2023-02-15 11:23:30 -0800

    Complete Function

---
+++ TwoNumberSum/src/TwoNumberSum.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoNumberSum {
+    public static void main(String[] args) {
+
+        int array[] = {3, 5, -4, 8, 11, 1, -1, 6};
+        int targetSum = 10;
+        int result[] = twoNumberSum(array, targetSum);
+        Set<Integer> map = new HashSet<>();
+        for (int i = 0; i < result.length; i++) {
+            map.add(result[i]);
+        }
+        System.out.println(map);
+    }
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+
+        Set<Integer> map = new HashSet<>();
+
+        for (int i = 0; i < array.length; i++) {
+            int x = targetSum - array[i];
+            if (map.contains(x)) {
+                return new int[]{x, array[i]};
+            } else {
+                map.add(array[i]);
+            }
+        }
+        return new int[0];
+    }
+}
