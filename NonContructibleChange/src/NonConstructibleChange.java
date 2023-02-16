660daef17e0b5d74784bb0f3239c6f1c0bbe0236
Archana Garudanagiri Vidyashankar <gvarchana95@gmail.com>
2023-02-15 17:36:49 -0800

    NonConstructibleChange

---
+++ NonContructibleChange/src/NonConstructibleChange.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class NonConstructibleChange {
+    public static void main(String[] args) {
+
+        int[] coins = {5, 7, 1, 1, 2, 3, 22};
+
+        int result = nonConstructibleChange(coins);
+
+        System.out.println(result);
+    }
+
+    public static int nonConstructibleChange(int[] coins) {
+
+        int change = 0;
+        Arrays.sort(coins);
+
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] > change + 1) {
+                return change + 1;
+            } else {
+                change = change + coins[i];
+            }
+        }
+        return change + 1;
+    }
+}
